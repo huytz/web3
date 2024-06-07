246a320f8fbf6205b79e1e7cbc28039b245dbef9
Huy Trần <huytz@Huys-MacBook-Air.local>
2024-06-07 14:33:19 +0700

    Init.

---
+++ evm-wallet/create-wallets.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const ethers = require("ethers");
+
+let existingData = [];
+
+let numberWallet = 2 
+
+for (let i = 0; i < numberWallet; i++) {
+    const wallet = ethers.Wallet.createRandom();
+    const walletObj = {
+        "seedPhrase": wallet.mnemonic.phrase,
+        "address": wallet.address.toLowerCase(),
+        "privateKey": wallet.privateKey
+    };
+    existingData.push(walletObj);
+}
+
+fs.appendFile('./wallets.json', JSON.stringify(existingData, null, 2),(err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        // Get the file contents after the append operation 
+        console.log("\nFile Contents of file after append:")
+    }
+});
